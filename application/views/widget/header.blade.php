4fb71ca452e027441e1da4633d95c93c30a1c280
DESKTOP-073RIC6\daotran <lytranuit@gmail.com>
2017-11-08 22:27:29 +0700

    slider

--- application/views/widget/header.blade.php
+++ application/views/widget/header.blade.php
@@ -4,41 +4,39 @@
 <link rel="stylesheet" href="{{base_url()}}public/assets/mobirise/css/mbr-additional.css" type="text/css">
 <section id="ext_menu-e">
 
-    <nav class="navbar navbar-dropdown navbar-fixed-top ">
+
+    <nav class="navbar navbar-dropdown navbar-fixed-top" style="width: 100%;margin: 0;">
         <div class="container">
+            <div class="row">
+                <div class="mbr-table">
+                    <div class="mbr-table-cell">
+                        <div class="navbar-brand">
+                            <a href="{{base_url()}}" class="navbar-logo"><img src="{{base_url()}}public/images/local/logo-146x128.png" alt="Mobirise"></a>
+                            <!--<a class="navbar-caption text-white" href="{{base_url()}}">CELADONCITY<br></a>-->
+                        </div>
 
-            <div class="mbr-table">
-                <div class="mbr-table-cell">
-
-                    <div class="navbar-brand">
-                        <a href="{{base_url()}}" class="navbar-logo"><img src="{{base_url()}}public/images/local/logo-146x128.png" alt="Mobirise"></a>
-                        <!--<a class="navbar-caption text-white" href="{{base_url()}}">CELADONCITY<br></a>-->
                     </div>
+                    <div class="mbr-table-cell">
 
-                </div>
-                <div class="mbr-table-cell">
+                        <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                            <div class="hamburger-icon"></div>
+                        </button>
+                        <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">
+                            <li class="nav-item">
+                                <a class="nav-link link" href="{{base_url()}}">TRANG CHỦ</a>
+                            </li>
+                            <?= $menu ?> 
+                        </ul>
+                        <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                            <div class="close-icon"></div>
+                        </button>
 
-                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
-                        <div class="hamburger-icon"></div>
-                    </button>
-                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">
-                        <li class="nav-item">
-                            <a class="nav-link link" href="{{base_url()}}">TRANG CHỦ</a>
-                        </li>
-                        <?= $menu ?> 
-                    </ul>
-                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
-                        <div class="close-icon"></div>
-                    </button>
-
+                    </div>
                 </div>
             </div>
-
         </div>
     </nav>
-
 </section>
-
 <script src="{{base_url()}}public/js/jquery.validate.js"></script>
 <script src="{{base_url()}}public/assets/smooth-scroll/smooth-scroll.js"></script>
 <script src="{{base_url()}}public/assets/dropdown/js/script.min.js"></script>
--- application/views/widget/slider.blade.php
+++ application/views/widget/slider.blade.php
@@ -1,22 +1,26 @@
+<script type="text/javascript" src="{{base_url()}}sliderengine/jquery.hislider.js"></script>
 
 <!-- === MAIN Background === -->
 <div class="slide story" id="slide-1" data-slide="1">
-    <a href="{{base_url()}}" style="position: absolute;z-index: 1000;top: 80px;left: 0px;width: 100%;">
-        <img src="{{base_url()}}public/images/local/logo.png" alt="<?= config_item('hotline') ?>" style="width: 150px;" class="onlyone" data-style='bounceInLeft'>
-        <br>
-        <span class="hotline onlyone" data-style="fadeIn" style="animation-delay: 1s;">
-            <?= config_item('hotline') ?>
-        </span>
-    </a>
-    <!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
-    <div id="wowslider-container1">
-        <div class="ws_images"><ul>
-                @foreach($arr_slider as $key => $slider)
-                <li><img src="{{base_url(). $slider['hinhanh']}}" alt="" title="$key" id="wows1_<?= $key ?>"/></li>
-                @endforeach
-            </ul></div>
-    </div>	
-    <script type="text/javascript" src="{{base_url()}}public/engine1/wowslider.js"></script>
-    <script type="text/javascript" src="{{base_url()}}public/engine1/script.js"></script>
-    <!-- End WOWSlider.com BODY section -->
-</div><!-- /slide1 -->
+    <div id="effect" style="display: flex;align-items: center;position: absolute; width: 100%;height: 100%;min-height: 540px;" class='hidden-md-down'>
+        <a href="#" style="margin: auto;z-index: 1000;">
+            <img src="{{base_url()}}public/images/local/logo.png" alt="<?= config_item('hotline') ?>" style="width: 150px;" class="onlyone img-responsive" data-style='bounceInLeft'>
+            <br>
+            <span class="hotline onlyone" data-style="fadeIn" style="animation-delay: 1s;">
+                <?= config_item('hotline') ?>
+            </span>
+        </a>
+    </div>
+    <div id='hislider1' style="max-width:2000px;  max-height:800px; margin: 0 auto;">
+        <ul style="display: none;overflow: hidden; height: 0; visibility: hidden; opacity: 0;">
+            @foreach($arr_slider as $key => $slider)
+            <li>
+                <div>
+                    <img data-src="{{base_url(). $slider['hinhanh']}}" title="" alt="" data-content-type="image" data-content="" data-interval="-1"/>
+                    <div data-type="effect" data-effect-type="Random"></div>
+                </div>
+            </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
